5af223fbc3e1811293f31f93ddec0145ccc9392e
oliver.lu <oliver.lu@gmail.com>
2022-12-14 22:21:16 +0800

    VNC mouse整合main程式進行調整

--- main/src/main.c
+++ main/src/main.c
@@ -10,15 +10,16 @@
 #define _DEFAULT_SOURCE /* needed for usleep() */
 #include <stdlib.h>
 #include <unistd.h>
-#define SDL_MAIN_HANDLED /*To fix SDL's "undefined reference to WinMain" issue*/
-#include <SDL2/SDL.h>
-#include "lvgl/lvgl.h"
-#include "lvgl/examples/lv_examples.h"
-#include "lv_examples/lv_demo.h"
+#define SDL_MAIN_HANDLED /*To fix SDL's "undefined reference to WinMain"       \
+                            issue*/
 #include "lv_drivers/display/monitor.h"
-#include "lv_drivers/indev/mouse.h"
 #include "lv_drivers/indev/keyboard.h"
+#include "lv_drivers/indev/mouse.h"
 #include "lv_drivers/indev/mousewheel.h"
+#include "lv_examples/lv_demo.h"
+#include "lvgl/examples/lv_examples.h"
+#include "lvgl/lvgl.h"
+#include <SDL2/SDL.h>
 
 #include "lv_drivers/vncserver/vncserver.h"
 
@@ -68,8 +69,7 @@ static int tick_thread(void *data);
  *   GLOBAL FUNCTIONS
  **********************/
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   (void)argc; /*Unused*/
   (void)argv; /*Unused*/
 
@@ -79,31 +79,31 @@ int main(int argc, char **argv)
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
   hal_init();
 
-//  lv_example_switch_1();
-//  lv_example_calendar_1();
-//  lv_example_btnmatrix_2();
-//  lv_example_checkbox_1();
-//  lv_example_colorwheel_1();
-//  lv_example_chart_6();
-//  lv_example_table_2();
-//  lv_example_scroll_2();
-//  lv_example_textarea_1();
-//  lv_example_msgbox_1();
-//  lv_example_dropdown_2();
-//  lv_example_btn_1();
-//  lv_example_scroll_1();
-//  lv_example_tabview_1();
-//  lv_example_tabview_1();
-//  lv_example_flex_3();
-//  lv_example_label_1();
+  //  lv_example_switch_1();
+  //  lv_example_calendar_1();
+  //  lv_example_btnmatrix_2();
+  //  lv_example_checkbox_1();
+  //  lv_example_colorwheel_1();
+  //  lv_example_chart_6();
+  //  lv_example_table_2();
+  //  lv_example_scroll_2();
+  //  lv_example_textarea_1();
+  //  lv_example_msgbox_1();
+  //  lv_example_dropdown_2();
+  //  lv_example_btn_1();
+  //  lv_example_scroll_1();
+  //  lv_example_tabview_1();
+  //  lv_example_tabview_1();
+  //  lv_example_flex_3();
+  //  lv_example_label_1();
 
   lv_demo_widgets();
-//  lv_demo_keypad_encoder();
-//  lv_demo_benchmark();
-//  lv_demo_stress();
-//  lv_demo_music();
+  //  lv_demo_keypad_encoder();
+  //  lv_demo_benchmark();
+  //  lv_demo_stress();
+  //  lv_demo_music();
 
-  while(1) {
+  while (1) {
     /* Periodically call the lv_task handler.
      * It could be done in a timer interrupt or an OS task too.*/
     lv_timer_handler();
@@ -116,14 +116,21 @@ int main(int argc, char **argv)
 /**********************
  *   STATIC FUNCTIONS
  **********************/
+void hal_mouse_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data) {
+
+  if (vnc_mouse_read(indev_drv, data) == 1) {
+    update_mouse(data->point.x, data->point.y, data->state);
+  }
+  mouse_read(indev_drv, data);
+}
 
 /**
  * Initialize the Hardware Abstraction Layer (HAL) for the LVGL graphics
  * library
  */
-static void hal_init(void)
-{
-  /* Use the 'monitor' driver which creates window on PC's monitor to simulate a display*/
+static void hal_init(void) {
+  /* Use the 'monitor' driver which creates window on PC's monitor to simulate a
+   * display*/
   monitor_init();
   /* Tick init.
    * You have to call 'lv_tick_inc()' in periodically to inform LittelvGL about
@@ -145,15 +152,16 @@ static void hal_init(void)
   disp_drv.ver_res = MONITOR_VER_RES;
   disp_drv.antialiasing = 1;
 
-  lv_disp_t * disp = lv_disp_drv_register(&disp_drv);
+  lv_disp_t *disp = lv_disp_drv_register(&disp_drv);
 
-  lv_theme_t * th = lv_theme_default_init(disp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), LV_THEME_DEFAULT_DARK, LV_FONT_DEFAULT);
+  lv_theme_t *th = lv_theme_default_init(
+      disp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED),
+      LV_THEME_DEFAULT_DARK, LV_FONT_DEFAULT);
   lv_disp_set_theme(disp, th);
 
   vncserver_init();
 
-
-  lv_group_t * g = lv_group_create();
+  lv_group_t *g = lv_group_create();
   lv_group_set_default(g);
 
   /* Add the mouse as input device
@@ -163,8 +171,9 @@ static void hal_init(void)
   lv_indev_drv_init(&indev_drv_1); /*Basic initialization*/
   indev_drv_1.type = LV_INDEV_TYPE_POINTER;
 
-  /*This function will be called periodically (by the library) to get the mouse position and state*/
-  indev_drv_1.read_cb = mouse_read;
+  /*This function will be called periodically (by the library) to get the mouse
+   * position and state*/
+  indev_drv_1.read_cb = hal_mouse_read;
   lv_indev_t *mouse_indev = lv_indev_drv_register(&indev_drv_1);
 
   keyboard_init();
@@ -180,14 +189,16 @@ static void hal_init(void)
   indev_drv_3.type = LV_INDEV_TYPE_ENCODER;
   indev_drv_3.read_cb = mousewheel_read;
 
-  lv_indev_t * enc_indev = lv_indev_drv_register(&indev_drv_3);
+  lv_indev_t *enc_indev = lv_indev_drv_register(&indev_drv_3);
   lv_indev_set_group(enc_indev, g);
 
   /*Set a cursor for the mouse*/
   LV_IMG_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
-  lv_obj_t * cursor_obj = lv_img_create(lv_scr_act()); /*Create an image object for the cursor */
-  lv_img_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
-  lv_indev_set_cursor(mouse_indev, cursor_obj);             /*Connect the image  object to the driver*/
+  lv_obj_t *cursor_obj =
+      lv_img_create(lv_scr_act()); /*Create an image object for the cursor */
+  lv_img_set_src(cursor_obj, &mouse_cursor_icon); /*Set the image source*/
+  lv_indev_set_cursor(mouse_indev,
+                      cursor_obj); /*Connect the image  object to the driver*/
 }
 
 /**
@@ -198,7 +209,7 @@ static void hal_init(void)
 static int tick_thread(void *data) {
   (void)data;
 
-  while(1) {
+  while (1) {
     SDL_Delay(5);
     lv_tick_inc(5); /*Tell LittelvGL that 5 milliseconds were elapsed*/
   }
